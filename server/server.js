c2e334d08068db423fcb1eddadf4d6383c628087
a1k2f3 <164070836+a1k2f3@users.noreply.github.com>
2024-08-18 17:32:33 +0500

    Update server.js
    hh

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors(
   {
-    origin:["//https://final-portfolio-c7zy.vercel.app/contact"],
+    origin:["https://final-portfolio-c7zy.vercel.app/contact"],
     methods:["POST,GET"],
     credentials:true
   }
